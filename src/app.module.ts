b393555429856577fd7a50814afe334b08fd51d6
Koravit Kaewlek <koravit258@gmail.com>
2021-10-01 23:40:36 +0700

    run nest g res modules/{name} --no-spec

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ProductModule } from './modules/product/product.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
+    ProductModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
